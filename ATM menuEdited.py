07198abeceec0319372923e760c01c399686b252
peterk189 <ctw20.peter.kelly@corketb.ie>
2024-09-15 21:11:22 +0300

    Add files via upload

---
+++ ATM menuEdited.py
@@ -0,0 +1,22 @@
+# Date: Sept 2024
+# Author: Ms Hayes
+# Purpose: This code displays an ATM menu
+print("\t|-------------------------------|")
+print("\t|\t LCCS BANK LIMITED\t|")
+print("\t|\t ATM Main Menu\t\t|")
+print("\t|\t\t\t\t|")
+print("\t|\t1. Balance Enquiry\t|")
+print("\t|\t2. Cash Lodgement\t|")
+print("\t|\t3. Cash Withdrawal\t|")
+print("\t|\t4. Cash Transfer\t|")
+print("\t|\t5. Change PIN\t\t|")
+print("\t|\t6. Other Services\t|")
+print("\t|\t7. Call a representative|")
+print("\t|\t\t\t\t|")
+print("\t|\t8. Exit\t\t\t|")
+print("\t|-------------------------------|")
+print("\t|\t\t\t\t|")
+print("\t| CHOOSE AN OPTION >> \t\t|")
+print("\t|\t\t\t\t|")
+print("\t|-------------------------------|")
+print("")
