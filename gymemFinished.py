65e7a0a5216a78bdf1a045aaf5c88501bbe237c0
peterk189 <ctw20.peter.kelly@corketb.ie>
2024-09-15 21:07:53 +0300

    Add files via upload

---
+++ gymemFinished.py
@@ -0,0 +1,20 @@
+#date : 15th sept 2024
+#Author : Peter k
+#design a menu
+
+print("\t|-------------------------|")
+print("\t|\tMidleton Gym \t  | ")
+print("\t|  \t             \t  | ")
+print("\t|  \t             \t  | ")
+print("\t|  1. 1 months = 40 euro  |")
+print("\t|  2. 3 months = 100 euro |")
+print("\t|  3. 6 months = 200 euro |")
+print("\t|  4. 12 months = 400 euro|")
+print("\t|  \t             \t  | ")
+print("\t|  \t             \t  | ")
+print("\t|  Contact - 123-456-789  | ")
+print("\t|      for more info \t  | ")
+print("\t|  \t             \t  | ")
+print("\t|  \t             \t  | ")
+print("\t|  \t        \t  | ")
+print("\t|-------------------------|")
