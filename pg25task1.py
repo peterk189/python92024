d05a5db53ee9460e991af9f02887ea2033f543ed
peterk189 <ctw20.peter.kelly@corketb.ie>
2024-09-19 22:22:31 +0100

    Add files via upload

---
+++ pg25task1.py
@@ -0,0 +1,13 @@
+#Ask user their favourite artist
+
+favourite_artist = input("enter the name of your favourite artist:")
+
+print("enter the name of your favourite artist:")
+
+#output
+
+print("Your favourite artist is",favourite_artist)
+
+#other variable
+
+print(favourite_artist,"is very good")
