4d70ed4a03a3c07e6a263ebc98b792f32e865e4c
peterk189 <ctw20.peter.kelly@corketb.ie>
2024-09-23 19:16:09 +0100

    Add files via upload

---
+++ challenge1-4.py
@@ -0,0 +1,57 @@
+#challenge 1-4 Onenote
+
+
+#Square
+# import turtle
+# for i in range(0,4):
+#     turtle.forward(100)
+#     turtle.right(90)
+#     
+# turtle.exitonclick()
+
+#octagon
+
+# import turtle
+# for i in range(0,8):
+#     turtle.forward(80)
+#     turtle.right(45)
+#     
+# turtle.exitonclick()
+
+
+#envelope
+
+# import turtle
+# for i in range(0,4):
+#     turtle.forward(100)
+#     turtle.right(90)
+#     
+# for i in range(0,1):
+#     turtle.right(45)
+#     turtle.forward(72)
+#     turtle.left(90)
+#     turtle.forward(72)
+# turtle.exitonclick()
+
+
+
+#Star
+# 
+# import turtle
+# for i in range(0,6):
+#     turtle.forward(100)
+#     turtle.right(144)
+#     
+# turtle.exitonclick()
+
+
+#challenge 4
+
+# import turtle
+# for i in range(0,100):
+#     turtle.forward(50)
+#     turtle.right(144)
+#     turtle.left(90)
+#     turtle.forward(50)
+#     
+# turtle.exitonclick()
